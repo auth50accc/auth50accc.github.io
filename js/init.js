8e8503835bc806263f6822c203f5d3e36d307e63
auth50accc <152940153+auth50accc@users.noreply.github.com>
2023-12-04 20:49:07 -0600

    Update CDN init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyCrHoVHPsrYHQ8HKah5UEttbQqHydegK0k",
-  authDomain: "autenfb2023-5d562.firebaseapp.com",
-  projectId: "autenfb2023-5d562",
-  storageBucket: "autenfb2023-5d562.appspot.com",
-  messagingSenderId: "1089728085495",
-  appId: "1:1089728085495:web:ec62b3c83d3d98f7ac42f2",
-  measurementId: "G-S1T5SCE6JP"
+  apiKey: "AIzaSyDRh3xL327jigrQ4RKhY-WKGko46uSJSrs",
+    authDomain: "auth50accc-7bba6.firebaseapp.com",
+    projectId: "auth50accc-7bba6",
+    storageBucket: "auth50accc-7bba6.appspot.com",
+    messagingSenderId: "40422624674",
+    appId: "1:40422624674:web:ac5b30056f4c5412a836fb",
+    measurementId: "G-LNGQ8FLJS5"
     });
